3e64351ada071f0488d502661c8300ba068fcf28
Gagulo <grzegorz.sokalski@hotmail.com>
2018-10-20 13:06:40 +0200

    Creating user table n adding them option

---
+++ SimpleCrud/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using SimpleCrud.Repositories;
+using System.Web.Mvc;
+using SimpleCrud.Entities;
+
+namespace SimpleCrud.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IPersonRepository _repository = new PersonRepository();
+
+        public ActionResult Index()
+        {
+            var userList = _repository.GetAllUsers();
+            return View(userList);
+        }
+
+        public ActionResult Edit(long id)
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            var user = new User();
+            return View();
+        }
+    }
+}
---
+++ SimpleCrud/Repositories/PersonRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleCrud.Entities;
+
+namespace SimpleCrud.Repositories
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private readonly static IList<User> _users = new List<User>()
+        {
+            new User()
+        {
+            Id = 1,
+            FirstName = "Agnieszka",
+            LastName = "Nazwisko",
+            DateOfBirth = new DateTime(1987,11,11)
+        },
+             new User()
+        {
+            Id = 1,
+            FirstName = "Ania",
+            LastName = "Jakas",
+            DateOfBirth = new DateTime(1990,10,5)
+        }
+        };
+
+
+        public void Add(User user)
+        {
+            _users.Add(user);
+        }
+
+        public IList<User> GetAllUsers()
+        {
+            return _users;
+        }
+
+        public User GetUser(long id)
+        {
+            return _users.SingleOrDefault(u => u.Id == id);
+        }
+
+        public void Update(User user)
+        {
+            var userToUpdate = _users.Single(u => u.Id == user.Id);
+
+            userToUpdate.DateOfBirth = user.DateOfBirth;
+            userToUpdate.FirstName = user.FirstName;
+            userToUpdate.LastName = user.LastName;
+
+            userToUpdate.IsActive = user.IsActive;
+        }
+    }
+}
